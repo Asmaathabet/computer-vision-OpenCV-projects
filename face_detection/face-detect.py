e32b12be94f373c833bd1b81eaf46f38c664187f
Asmaa Thabet <asmathabet2009@hotmail.com>
2026-01-13 12:57:54 +0100

    add face detection app

---
+++ face_detection/face-detect.py
@@ -0,0 +1,21 @@
+import cv2
+# print(cv2.__version__)
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+webcam = cv2.VideoCapture(0)
+while True:
+    _,img = webcam.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.2 , 4)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y), (x+w, y+h), (0,0,255), 4)  
+    cv2.imshow("Face detection", img)
+    key = cv2.waitKey(10)
+    # 27 --> escape
+    if key == 27:
+        break
+webcam.release()
+cv2.destroyAllWindows()
+
+
+
